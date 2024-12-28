6a81435275a8e39845524cdb3e1fd996305a0659
w212w <edaev.bilal@icloud.com>
2024-12-28 22:53:21 +0300

    added functions in clients, main. lib

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/w212w/ReminderBot
+
+go 1.23.4

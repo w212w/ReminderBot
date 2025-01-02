9da00409009e00cdc8082bea30a9e36d84c3c64c
w212w <edaev.bilal@icloud.com>
2025-01-03 02:47:17 +0300

    fixes

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
-	"github.com/w212w/ReminderBot/clients/telegram"
+	tgClient "github.com/w212w/ReminderBot/clients/telegram"
+	eventconsumer "github.com/w212w/ReminderBot/consumer/event-consumer"
+	"github.com/w212w/ReminderBot/events/telegram"
+	"github.com/w212w/ReminderBot/storage/sqlite"
 )
 
 const (
-	tgBotHost = "api.telegram.org"
+	tgBotHost         = "api.telegram.org"
+	sqliteStoragePath = "data/sqlite/storage.db"
+	batchSize         = 100
 )
 
 func main() {
-	tgClient := telegram.New(tgBotHost, mustToken())
+	//s := files.New(storagePath)
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	if err := s.Init(context.TODO()); err != nil {
+		log.Fatal("can't init storage: ", err)
+	}
+
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		s,
+	)
+
+	log.Print("service started")
+
+	consumer := eventconsumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
 }
 
 func mustToken() string {
 	token := flag.String(
-		"token-bot-token",
+		"tg-bot-token",
 		"",
 		"token for access to telegram bot",
 	)
@@ -29,5 +56,4 @@ func mustToken() string {
 	}
 
 	return *token
-
 }
